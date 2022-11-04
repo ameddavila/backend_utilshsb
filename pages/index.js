a8d3bece4e4e57e9c32e389d4890a724f4059591
@med <amed.dav@gmail.com>
2022-11-03 20:12:45 -0400

    Este es el cuarto  commit del proyeco.

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,15 @@ import WidgetBuscador from "../components/widgetBuscador"
 import WidgetFichas from "../components/widgetFichas"
 export default function Home() {
   return (
-      <div> Hola Putito ggg</div>
-   
+    <div className="grid">
+      <div className="col-9">
+        <WidgetBuscador></WidgetBuscador>
+      </div>
+      <div className="col-3">
+        <WidgetFichas></WidgetFichas>
+      </div>
+    </div>
+
 
   )
 }
